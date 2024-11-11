03e73a378e65cf97909f2cbc910e4642b35f0d53
do.thang <100296543+DTD2604@users.noreply.github.com>
2024-11-11 17:47:52 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,51 +7,57 @@ import cors from 'cors';
 import { serverConfig } from './config/sv.config';
 import { AppModule } from './persistence/db/connection.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import logger from './config/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap');
-
-  // config CORS
-  const host = serverConfig.server.host;
-  const port = serverConfig.server.port;
-  const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  };
-
-  app.use(cors(corsOptions));
-
-  // Middleware
-  app.use(bodyParser.json());
-  app.use(cookieParser());
-
-  // Swagger
-  const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('The API description for your NestJS app')
-    .setVersion('1.0')
-    .addBearerAuth(
-      // Thêm dòng này để cấu hình JWT token
-      {
-        type: 'http',
-        scheme: 'Bearer',
-        bearerFormat: 'JWT',
-        in: 'header',
-      },
-      'JWT-auth', // Tên này sẽ được dùng để tham chiếu khi thêm token
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  // connect to the database and listen for requests
   try {
-    SwaggerModule.setup('api', app, document);
-    await app.listen(port);
-    logger.log(`Swagger is running on ${host}/api`);
+    const app = await NestFactory.create(AppModule);
+    const logger = new Logger('Bootstrap');
+
+    // config CORS
+    const host = serverConfig.server.host;
+    const port = serverConfig.server.port;
+    const corsOptions = {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    };
+
+    app.use(cors(corsOptions));
+
+    // Middleware
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+
+    // Swagger
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('The API description for your NestJS app')
+      .setVersion('1.0')
+      .addBearerAuth(
+        // Thêm dòng này để cấu hình JWT token
+        {
+          type: 'http',
+          scheme: 'Bearer',
+          bearerFormat: 'JWT',
+          in: 'header',
+        },
+        'JWT-auth', // Tên này sẽ được dùng để tham chiếu khi thêm token
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    // connect to the database and listen for requests
+    try {
+      SwaggerModule.setup('api', app, document);
+      await app.listen(port);
+      logger.log(`Swagger is running on ${host}/api`);
+    } catch (err) {
+      logger.error('Unable to connect to the database:', err);
+    }
   } catch (err) {
-    logger.error('Unable to connect to the database:', err);
+    console.log('error main: ' + err);
+    logger.error('error main: ' + err);
   }
 }
 
